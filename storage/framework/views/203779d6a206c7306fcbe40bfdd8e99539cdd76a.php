bc78fa83d3168a6ac56476b8322b2b7b6ea13edb
Nubo <72999487+Nuboctane@users.noreply.github.com>
2022-04-18 11:39:00 +0200

    Add files via upload

---
+++ storage/framework/views/203779d6a206c7306fcbe40bfdd8e99539cdd76a.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+<body>
+
+<?php $__env->startSection('title', 'Contact'); ?>
+<?php $__env->startSection('content'); ?>
+   <h1>Contact form</h1>
+   <div id="contact">
+       <form action="/contact/send" method="post">
+           <?php echo csrf_field(); ?>
+           <input type="text" name="first_name" id="first_name" placeholder="first name">
+           <br>
+           <input type="submit" value="submit">
+       </form>
+   </div>
+<?php $__env->stopSection(); ?>
+</body>
+</html>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\resources\views/contact.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ca7c3f7176b5f427fe47fb1e53d6c974aa49992c.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <?php echo $__env->yieldContent('content'); ?>
+       <ul>
+           <li><a href="/home">Home</a></li>
+           <li><a href="/contact">Contact</a></li>
+       </ul>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\laravel\resources\views/layout.blade.php ENDPATH**/ ?>
